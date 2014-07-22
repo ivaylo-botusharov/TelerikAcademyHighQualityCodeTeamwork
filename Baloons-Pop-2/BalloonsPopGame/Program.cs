8c147dfb2d876595842d5d19972bc5d49d426db8
Martin Belev <martinbelev93@gmail.com>
2014-07-22 11:49:29 -0700

    Remove all read input and extract it to Game class.

--- Baloons-Pop-2/BalloonsPopGame/Program.cs
+++ Baloons-Pop-2/BalloonsPopGame/Program.cs
@@ -6,42 +6,7 @@
     {
         public static void Main()
         {
-            int playfieldSize = ConsoleIOEngine.ReadPlayfieldSize();
-            Playfield playfield = null;
-
-
-            bool isPlayfieldSizeIncorrect = true;
-
-            while (isPlayfieldSizeIncorrect)
-            {
-                switch (playfieldSize)
-                {
-                    case 1:
-                        SmallPlayfieldFactory smallPlayfieldFactory = new SmallPlayfieldFactory();
-                        playfield = smallPlayfieldFactory.CreatePlayfield();
-                        isPlayfieldSizeIncorrect = false;
-                        break;
-                    case 2:
-                        MediumPlayfieldFactory mediumPlayfieldFactory = new MediumPlayfieldFactory();
-                        playfield = mediumPlayfieldFactory.CreatePlayfield();
-                        isPlayfieldSizeIncorrect = false;
-                        break;
-                    case 3:
-                        LargePlayfieldFactory largePlayfieldFactory = new LargePlayfieldFactory();
-                        playfield = largePlayfieldFactory.CreatePlayfield();
-                        isPlayfieldSizeIncorrect = false;
-                        break;
-                    default:
-                        Console.WriteLine("You have entered incorrect field size");
-                        break;
-                }
-                
-            }
-
-            PopStrategy popStrategy = new RecursivePopStrategy();
-            Game game = new Game(playfield, popStrategy);
-
-            game.Start();
+            Game.Start();
         }
     }
 }
