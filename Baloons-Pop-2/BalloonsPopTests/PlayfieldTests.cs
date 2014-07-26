088cdfcb1fc17b5a2cd66192b0541e1b088bbc00
Tihomir Arahangelov <angelov.tisho@gmail.com>
2014-07-26 22:47:26 +0300

    Unit tests for Playfield class added. Testing for initial width and height, negative width and height, zero width and height.

---
+++ Baloons-Pop-2/BalloonsPopTests/PlayfieldTests.cs
@@ -0,0 +1,57 @@
+﻿namespace BalloonsPopTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BalloonsPop;
+
+    [TestClass]
+    public class PlayfieldTests
+    {
+        [TestMethod]
+        public void InitialHeightShouldBeTenForNewInstance()
+        {
+            Playfield playfield = new Playfield();
+            int expected = 10;
+            int actual = playfield.Height;
+
+            Assert.AreEqual(expected, actual, "When new instance is created without parameters, the height of the playfield should have value of 10.");
+        }
+
+        [TestMethod]
+        public void InitialWidthShouldBeFiveForNewInstance()
+        {
+            Playfield playfield = new Playfield();
+            int expected = 5;
+            int actual = playfield.Width;
+
+            Assert.AreEqual(expected, actual, "When new instance is created without parameters, the width of the playfield should have value of 5.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeHeightShouldThrowException()
+        {
+            Playfield playfield = new Playfield(-20);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeWidthShouldThrowException()
+        {
+            Playfield playfield = new Playfield(10, -20);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ZeroHeightShouldThrowException()
+        {
+            Playfield playfield = new Playfield(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ZeroWidthShouldThrowException()
+        {
+            Playfield playfield = new Playfield(10, 0);
+        }
+    }
+}
